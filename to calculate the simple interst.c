815d95cb309b44cb91d0abfb3fb24a7185361884
Aditya singh <rajsinght932@gmail.com>
2024-09-27 15:49:10 +0530

    5

---
+++ to calculate the simple interst.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+ void main( )
+{
+float a,b,c,d;
+printf("Please enter the principal amount,rate of interest,time period");
+scanf("%f%f%f",&a,&b,&c);
+d=(a*b*c)/100;
+printf("Your simplr interst is:%f",d);
+}
