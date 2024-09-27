7856bcb5a9840f5b80770389c1ac80b8130e8134
Aditya singh <rajsinght932@gmail.com>
2024-09-27 21:29:00 +0530

    8

---
+++ To calculate the area of triangle using heron formula.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+float a,b,c,s,area;
+printf("Please enter the length of 3 side of the triangle.\n:");
+scanf("%f%f%f",&a,&b,&c);
+s=(a+b+c)/2;
+printf("Semiperimeter of the give triangle is:%f\n",s);
+area=pow((s*(s-a)*(s-b)*(s-c)),1/2);
+printf("Area of the give triangle is: %f\n",area);
+}
