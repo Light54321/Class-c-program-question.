9f5a125c4e05f3756500df0043c21410a0365bf5
Aditya singh <rajsinght932@gmail.com>
2024-10-04 15:58:29 +0530

    7

--- to find the size of int float .c
+++ to find the size of int float .c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-int main() {
-    int intType;
-    float floatType;
-    // sizeof evaluates the size of a variable
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-   
-    return 0;
-}
+    void main()
+    {
+     int car ;
+    float  boy ;
+    double toy  ;
+    char cry;
+      // sizeof evaluates the size of a variable
+    printf("Size of int: %u bytes\n", sizeof(car));
+    printf("Size of float: %u bytes\n", sizeof(boy));
+    printf("Size of double: %u bytes\n", sizeof(toy));
+    printf("Size of char: %u byte\n", sizeof(cry));
+    printf(" done ");
+    return ;
+    }
+    
