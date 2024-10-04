7e2a2101ec90ab8a5b7a309d0f0890dada13ea3a
Aditya singh <rajsinght932@gmail.com>
2024-10-04 15:09:51 +0530

    5

--- to calculate the compoud interst.c
+++ to calculate the compoud interst.c
@@ -1,9 +1,9 @@
  #include<stdio.h>
  void main( )
 {
-float a,b,c,d;
+double a,b,c,d;
 printf("Please enter the principal amount,rate of interest,number of compound period");
-scanf("%f%f%f",&a,&b,&c);
+scanf("%lf%lf%lf",&a,&b,&c);
 d=a*(((1+b)*c)-a);
-printf("Your compound interst is:%f",d);
+printf("Your compound interst is:%lf",d);
 }
