77a18358fce13291ceb1ccc9c89cdee7509926ce
Aditya singh <rajsinght932@gmail.com>
2024-10-04 16:21:28 +0530

    9

---
+++ for calculating the value of celling and floor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    float a,b,c;
+    printf("Enter the number:\n");
+    scanf("%f",&c);
+    a=ceil(c);
+    b=floor(c);
+    printf(" celing of number:%f\n",a);
+     printf(" floor of number:%f\n",b);
+}
