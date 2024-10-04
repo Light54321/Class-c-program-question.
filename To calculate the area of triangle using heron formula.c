32d062be4b6ccdc78d3ebafa7e444ead78d43af5
Aditya singh <rajsinght932@gmail.com>
2024-10-04 15:06:15 +0530

    4

--- To calculate the area of triangle using heron formula.c
+++ To calculate the area of triangle using heron formula.c
@@ -7,7 +7,7 @@ printf("Please enter the length of 3 side of the triangle.\n:");
 scanf("%f%f%f",&a,&b,&c);
 s=(a+b+c)/2;
 printf("Semiperimeter of the give triangle is:%f\n",s);
-area=pow((s*(s-a)*(s-b)*(s-c)),1/2);
+area=pow((s*(s-a)*(s-b)*(s-c)),0.5);
 /* or you can use this line in place of  above line (area= sqrt(s*(s-a)*(s-b)*(s-c));).
 But only for square  not for cube or any other power .*/
 printf("Area of the give triangle is: %f\n",area);
