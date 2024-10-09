2d6569b9ab6a5b4b3e5a53cbfe11c08e5cb5f198
Aditya singh <rajsinght932@gmail.com>
2024-10-09 09:08:26 +0530

    4

--- to calculate the hypotenudse of a right angle triangle.c
+++ to calculate the hypotenudse of a right angle triangle.c
@@ -6,5 +6,5 @@ float a,b,c;
 printf("Please enter the height and base of given triangle:\n");
 scanf("%f%f",&a,&c);
 c=pow( pow(a,2)+pow(c,2),0.5);
-printf("The hypotenuse of the given triangle is %f:",c);
+printf("The hypotenuse of the given triangle is%f:",c);
 }
---
+++ to check weither the give number is odd or even.c
@@ -0,0 +1,8 @@
+ #include<stdio.h>
+ void main( )
+{
+ int a;
+ printf("Pleas enter the  number:\n");
+ scanf("%d",&a);
+ a%2==0?printf("number is even."):printf("number is odd.");
+}
